7405ada23108e9f11df47a278bd9996cc22ae1be
Davide Bellettini <davide.bellettini@gmail.com>
2013-02-22 16:15:53 +0100

    Add "find by name" API

--- phpBB/silex/src/controllers/forum.php
+++ phpBB/silex/src/controllers/forum.php
@@ -8,6 +8,28 @@ $forum->get('/{id}', function($id) use($app) {
     return $app->json($app['acp.forums']->get_forum_info($id));
 })->assert('id', '\d+');
 
+$forum->get('/{name}', function(Silex\Application $app, $name){
+    $db = $app['db'];
+    $table = FORUMS_TABLE;
+    
+    $query = <<<EOT
+SELECT *
+    FROM $table
+    WHERE
+        forum_name = '{$db->sql_escape($name)}'
+EOT;
+    
+    $db->sql_query($query);
+    
+    $rows = array();
+    
+    while(false !== ($row = $db->sql_fetchrow())) {
+        $rows[] = $row;
+    }
+    
+    return $app->json($rows);
+});
+
 $forum->post('', function(Request $request) use ($app){
     $forum_data['prune_days'] = $forum_data['prune_viewed'] = $forum_data['prune_freq'] = 0;
     $forum_data['forum_topics_per_page'] = 0;
